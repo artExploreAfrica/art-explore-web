f4f405ce45a5d9caaea74a2b9853f2902e190e3d
Mhirax <olowookeremiracle14@gmail.com>
2026-08-01 17:01:15 +0100

    merge super admin file to the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
+import { AdminRoutes } from './admin/routes';
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/admin/*" element={<AdminRoutes />} />
+    </Routes>
+  );
 }
 
 export default App;
--- frontend/src/superadminPanel/src/main.tsx
+++ frontend/src/superadminPanel/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./admin/AuthContext";
+import { AuthProvider } from "../../admin/AuthContext";
 import { App } from "./App";
 import "./index.css";
 
